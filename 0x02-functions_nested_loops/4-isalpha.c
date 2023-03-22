9a0343dbff828041cb3968d04350e966f4ae569e
Cherno Jallow <maticjallow08@gmail.com>
2023-03-22 18:30:58 +0000

    Check for uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - detect uppercase characters
+ * @c: variable to be input by user
+ * Return: 1 if lowercase or 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
