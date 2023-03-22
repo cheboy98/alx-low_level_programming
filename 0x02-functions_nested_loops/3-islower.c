797bc7ed934282a7a59b5d9874749d068a6955f0
Cherno Jallow <maticjallow08@gmail.com>
2023-03-22 18:17:33 +0000

    Check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - detect lowercase characters
+ * @c: variable to be input by user
+ * Return: 1 if lowercase or 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
