651502d40a9b7f719ab4bda37455c244c876b421
Cherno Jallow <maticjallow08@gmail.com>
2023-03-22 16:14:18 +0000

    Lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
