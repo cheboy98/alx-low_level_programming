3eaff8a0dc8b2417875d9e9827f866ea72b062ae
Cherno Jallow <maticjallow08@gmail.com>
2023-03-22 14:27:04 +0000

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * main - Entry point
+ * description - print _putchar
+ * Return: 0 for succes
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
