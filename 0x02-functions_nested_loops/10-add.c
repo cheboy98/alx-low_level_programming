ec648b80f3cde65f1ff0fb1cdf6a68ce529b5dad
Cherno Jallow <maticjallow08@gmail.com>
2023-03-23 02:23:07 +0000

    Add int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two int
+ * @n: int to be added
+ * @m: int to be added
+ * Return: Always 0
+ */
+
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+}
