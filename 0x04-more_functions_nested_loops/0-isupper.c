33e6d8a60c31c44b311220ec6ab056ca529635c4
Cherno Jallow <maticjallow08@gmail.com>
2023-03-23 23:26:20 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - computer uppercase characters
+ * @c: checked value
+ * Return: 1 is upper or 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int c = 65;
+
+	if (c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
