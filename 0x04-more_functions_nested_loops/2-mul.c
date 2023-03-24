68c317122f8e62d1f9dec16cc1bb22aee22a96c7
Cherno Jallow <maticjallow08@gmail.com>
2023-03-24 00:14:59 +0000

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply digit
+ * @a: first digit
+ * @b: second digit
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
