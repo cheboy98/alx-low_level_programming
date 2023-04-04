c8a8e40cb0f9a2b7884d235590e46f4f608ac194
Cherno Jallow <maticjallow08@gmail.com>
2023-04-04 13:05:06 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy to dest
+ * @dest: dest array
+ * @src: src array
+ * @n: size
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p;
+	unsigned int i;
+
+	p = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		p[i] = *src;
+	}
+
+	return (dest);
+}
