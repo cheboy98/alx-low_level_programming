93769956632d2d64c81688c5c6f36e25355807a1
Cherno Jallow <maticjallow08@gmail.com>
2023-04-06 03:19:19 +0000

    Compute the power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - compute the power of a number
+ * @x: base value
+ * @y: expo value
+ *
+ * Return: result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
