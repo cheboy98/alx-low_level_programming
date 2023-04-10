e5e204d8e1dcb673ebdfc9e4f390338f400417a3
Cherno Jallow <maticjallow08@gmail.com>
2023-04-10 17:35:12 +0000

    Add args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add the args
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= 'a' && *argv[i] <= 'z')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				int dig = atoi(argv[i]);
+
+				sum += dig;
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
+
