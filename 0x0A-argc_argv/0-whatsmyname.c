28297724f90273a2abd075e8ae0f51cd01d877c4
Cherno Jallow <maticjallow08@gmail.com>
2023-04-10 13:36:41 +0000

    Name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - print the name of the program
+ * @argc: number of args to the program
+ * @argv: array that holds the args of the program
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv);
+	return (0);
+}
