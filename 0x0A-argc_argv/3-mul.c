ab930655a96092bdfb6d664efd72518f10542490
Cherno Jallow <maticjallow08@gmail.com>
2023-04-10 16:14:46 +0000

    Multiply args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,19 @@ int main(int argc, char *argv[])
 {
 	int mul, arg1, arg2;
 
-	if (argc < 3 || argc > 3)
+	if (argc <= 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[2]);
-	mul = arg1 * arg2;
+	else
+	{
+		arg1 = atoi(argv[1]);
+		arg2 = atoi(argv[2]);
+		mul = arg1 * arg2;
 
-	printf("%d\n", mul);
+		printf("%d\n", mul);
+	}
 	return (0);
 }
