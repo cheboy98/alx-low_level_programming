d0e9c486df207fb20d0f02547a2ae4ebfec93d83
Cherno Jallow <maticjallow08@gmail.com>
2023-04-12 00:49:51 +0000

    Copy string to dynamic memory location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - dynamically allocate memory
+ * @str: value to store
+ *
+ * Return: str
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc(sizeof(*str));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < sizeof(str) - 1; i++)
+	{
+		s[i] = str[i];
+	}
+
+	return (s);
+}
