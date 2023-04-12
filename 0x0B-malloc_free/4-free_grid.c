d61e48a80b93a6d47443fc3cf884dd912a08fa20
Cherno Jallow <maticjallow08@gmail.com>
2023-04-12 23:29:07 +0000

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free memory
+ * @grid: 2D array
+ * @height: array height
+ *
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+	return (0);
+}
