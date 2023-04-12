99dc4eda7f1ba7c7cb6911e26e43761c3088a058
Cherno Jallow <maticjallow08@gmail.com>
2023-04-12 02:24:07 +0000

    Concat strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,12 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
@@ -21,12 +27,6 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	while (s2[j] != '\0')
 		j++;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
 
 	s = malloc(sizeof(char) * (i + j + 1));
 
