d7368768f8b667c776b9e18444ce84910532eefb
Cherno Jallow <maticjallow08@gmail.com>
2023-06-07 20:57:27 +0000

    print a string using recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,9 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	else if (s == "")
+	//if (*s == "")
+		//return;
+	else if (*s == '\0')
 		return;
 	_putchar(*s++);
 	_puts_recursion(s);
