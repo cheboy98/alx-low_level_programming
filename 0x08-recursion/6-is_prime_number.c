6774e09993c29a71dbe0f32b1e503cc1e686cb50
Cherno Jallow <maticjallow08@gmail.com>
2023-06-08 21:34:25 +0000

    Compute if number is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int helper_is_prime_number(int n, int div);
+/**
+ * is_prime_number - Compute if number is prime or not
+ * @n: Compute number
+ * Return: 1 if true and 0 is false
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (helper_is_prime_number(n, 1));
+}
+
+/**
+ * helper_is_prime_number - helper function
+ * @n: compute number
+ * @div: divisors
+ * 
+ * Return: 1 if true and 0 if false
+ */
+
+int helper_is_prime_number(int n, int div)
+{
+	if (div == 1 && div == n)
+		return (1);
+	else if (n % div == 0 && div > 0)
+		return (0);
+	else
+		return (helper_is_prime_number(n, div + 1));
+}
