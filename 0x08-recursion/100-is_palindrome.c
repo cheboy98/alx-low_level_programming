407a3cc0e08a2e7e81b488d8b6f988e9f59a0459
Cherno Jallow <maticjallow08@gmail.com>
2023-06-08 22:31:24 +0000

    Compute if a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+int helper_is_palindrome(char *s, int start, int end);
+
+/**
+ * is_palindrome - check if string is palindrome or not
+ * @s: compute string
+ *
+ * Return: 1 if true and 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int length = strlen(s);
+	if (length == 0 || length == 1)
+		return (1);
+	return (helper_is_palindrome(s, 0, length - 1));
+}
+
+/**
+ * helper_is_palindrome - helper function
+ * @start: start of string
+ * @end: end of string
+ *
+ * Return: 1 if true and 0 if false
+ */
+
+int helper_is_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	else if (s[start] == s[end])
+		return (helper_is_palindrome(s, start + 1, end - 1));
+	else
+		return (0);
+}
