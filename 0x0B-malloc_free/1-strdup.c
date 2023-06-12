a47674530e00b25aac9f77a8366e9093ac643aac
Cherno Jallow <maticjallow08@gmail.com>
2023-06-12 22:12:19 +0000

    Duplicate a string {strdup}

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Copy a string to a new memory location allocated by malloc
+ * @str: String to be copied
+ *
+ * Return: Pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	int i, length;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+
+	length = strlen(str);
+
+	array = malloc(sizeof(char) * length);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+		array[i] = str[i];
+
+	return (array);
+}
