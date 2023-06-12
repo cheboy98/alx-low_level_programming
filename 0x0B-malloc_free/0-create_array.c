c10297a795fbbd185db44009434ca8e72ffc761e
Cherno Jallow <maticjallow08@gmail.com>
2023-06-12 21:50:01 +0000

    Create an array of chars using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	array = malloc(sizeof(char) * size);
-	
+
+	if (array == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
 
