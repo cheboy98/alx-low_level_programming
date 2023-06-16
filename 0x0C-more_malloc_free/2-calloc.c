74bc3a55cf51f11556c0fcfa59be275e72e625fb
Cherno Jallow <maticjallow08@gmail.com>
2023-06-16 14:35:21 +0000

    Custom calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory to an array of nmemb elements
+ * @nmemb: number of elements of the array
+ * @size: size of each elements
+ *
+ * Return: pointer to the array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *array;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(size * nmemb);
+
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		array[i] = 0;
+
+	return (array);
+}
