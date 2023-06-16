c12f0995c43a97a6e06e697b71a9df9d8bb9ab7b
Cherno Jallow <maticjallow08@gmail.com>
2023-06-16 13:10:56 +0000

    Concate strings up to n bytes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,21 +14,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *array;
+	unsigned int s1_length, size, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	int s1_length = strlen(s1);
-	int size = s1_length + n + 1;
+	s1_length = strlen(s1);
+	size = s1_length + n + 1;
 
 	array = malloc(sizeof(char) * size);
-
 	if (array == NULL)
 		return (NULL);
 
-	array = strncat(s1, s2, n);
+	for (i = 0; i < s1_length; i++)
+		array[i] = s1[i];
 
+	for (i = 0; i < n; i++)
+		array[s1_length + i] = s2[i];
+
+	array[size - 1] = '\0';
 	return (array);
 }
