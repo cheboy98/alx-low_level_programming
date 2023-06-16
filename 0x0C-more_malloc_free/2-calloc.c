c1b3d4041fe99a56f1cf0b4a61c3665156e06d51
Cherno Jallow <maticjallow08@gmail.com>
2023-06-16 15:00:17 +0000

    Custom calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,15 +14,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *array;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if ((nmemb || size) == 0)
 		return (NULL);
 
 	array = malloc(sizeof(size) * nmemb);
 
 	if (array == NULL)
 		return (NULL);
+
 	for (i = 0; i < nmemb; i++)
+	{
 		array[i] = 0;
+	}
 
 	return (array);
 }
