144ae0dabe08f60633c2972544a872d9225a799a
Cherno Jallow <maticjallow08@gmail.com>
2023-06-20 01:36:56 +0000

    A simple structure in C

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Structure that stores the caracteristics of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
