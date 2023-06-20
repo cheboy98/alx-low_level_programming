8a1fd138f57347dd539c6a06bc941f41d6a328e6
Cherno Jallow <maticjallow08@gmail.com>
2023-06-20 20:09:22 +0000

    free struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.H>
+
+/**
+ * free_dog - free the dog struct from memory
+ * @d: pointer to the dog struct
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
