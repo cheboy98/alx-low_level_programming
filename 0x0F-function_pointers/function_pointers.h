b1ce0112ebb8a3bd5ccb4863f1259adf38d70997
Cherno Jallow <maticjallow08@gmail.com>
2023-06-21 18:43:30 +0000

    Project Header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#include <stddef.h>
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+
+#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
