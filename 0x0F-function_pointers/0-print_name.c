eb62b8a28933bbd2e0613fb12968500dc60801b4
Cherno Jallow <maticjallow08@gmail.com>
2023-06-21 18:09:51 +0000

    Print a name using function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: name of the person
+ * @f: function pointer
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
