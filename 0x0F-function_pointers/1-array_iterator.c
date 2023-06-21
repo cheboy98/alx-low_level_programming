cfe366ecf339e41fa9d814985e8f7cba3bc871ad
Cherno Jallow <maticjallow08@gmail.com>
2023-06-21 19:10:59 +0000

    Execute a function through a function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Execute a function passed as argument
+ * @array: array of ints
+ * @size: size of the array
+ * @action: function pointer
+ *
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size == 0 || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
